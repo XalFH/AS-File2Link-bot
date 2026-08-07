8d8b2520c8672184ff7ba15225efe5ac9d8aede4
AaryaN Sah <gudiyakum678@gmail.com>
2026-08-07 13:17:43 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+API_ID = 32541562
+API_HASH = "e37e4432298d5a5eb4a6e32c18804283"
+BOT_TOKEN = "7886130082:AAE1IROKiE8rvDvX2aDK-En-2p_rnzkcX_Q"
+MONGO_URI = "mongodb+srv://aaryansah954:QgDQRgyD7VUa7Eho@cluster0.wjo9zfm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+ADMIN_ID = 7006602588
+
+PORT = int(os.environ.get("PORT", 8080))
+URL = os.environ.get("RENDER_EXTERNAL_URL", "https://as-file2link-bot.onrender.com").rstrip('/')
