c0431f53a032c93ff8834022326e67a18866c54a
AaryaN Sah <gudiyakum678@gmail.com>
2026-08-07 13:07:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 API_ID = 32541562
 API_HASH = "e37e4432298d5a5eb4a6e32c18804283"
 BOT_TOKEN = "7886130082:AAE1IROKiE8rvDvX2aDK-En-2p_rnzkcX_Q"
-MONGO_URI = "mongodb+srv://aryankumarsha20:CjdV5plwbpvwTTCU@cluster0.3zw5xk8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://aaryansah954:QgDQRgyD7VUa7Eho@cluster0.wjo9zfm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 ADMIN_ID = 7006602588
 
 PORT = int(os.environ.get("PORT", 8080))
